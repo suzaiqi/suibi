83f657f19dcefd1a0cf285af550a00dae54e1b7b
DESKTOP-97ROPK1\suhua <suhuanqi@csii.com.cn>
2023-01-29 17:36:15 +0800

    练习代码提交

---
+++ vue3-ts/src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div>开始学习vue和ts</div>
+    <div @click="goAbout">跳转详情</div>
+  </div>
+</template>
+
+<script lang='ts'>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+import { defineComponent } from "vue";
+
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  setup() {
+
+    const router = useRouter();
+
+    // components: {
+    //   HelloWorld
+    // }
+
+    // let that = this;
+    const goAbout = (): void => {
+      
+      console.log(router, "===this");
+      router.push("studyView");
+    };
+    return {
+      goAbout,
+    };
+  },
+});
+</script>
---
+++ vue3-ts/src/views/studyView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div @click="goBack">学习跳转</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+    setup() {
+        const router = useRouter();
+        const goBack = (): void =>{
+            router.back();
+        }
+        return {
+            goBack
+        }
+    },
+})
+</script>
